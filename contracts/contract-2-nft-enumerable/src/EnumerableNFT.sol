3c300c7e9c242e90ec594e6574cc7fccec63ea48
Sergi Roca Laguna <sergiroca@163.com>
2024-05-12 18:06:00 +0200

    enumberable nft minimum solution

--- contracts/contract-2-nft-enumerable/src/EnumerableNFT.sol
+++ contracts/contract-2-nft-enumerable/src/EnumerableNFT.sol
@@ -2,15 +2,18 @@
 pragma solidity 0.8.24;
 import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//Create a new NFT collection with 20 items using ERC721Enumerable. The token ids should be [1..100] inclusive.
+
 contract EnumerableNFT is ERC721, IERC721Enumerable {
     error EnumerableNFT__OutOfBoundsIndex(uint256 index);
+    error EnumerableNFT__TotalSupplyReached();
+    error EnumerableNFT__OutOfBoundsTokenId();
 
     mapping(address => uint256[]) private s_ownedTokens;
     mapping(uint256 => uint256) private s_ownedTokensIndex;
 
     uint256[] private s_allTokens;
     mapping(uint256 => uint256) private s_allTokensIndex;
+    uint256 public constant MAX_SUPPLY = 20;
 
     constructor() ERC721("EnumerableNFT", "ENFT") {}
 
@@ -18,6 +21,21 @@ contract EnumerableNFT is ERC721, IERC721Enumerable {
 
     function totalSupply() public view override returns (uint256) {
         return s_allTokens.length;
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        if (totalSupply() >= 20) {
+            revert EnumerableNFT__TotalSupplyReached();
+        }
+
+        if (tokenId == 0 || tokenId > MAX_SUPPLY) {
+            revert EnumerableNFT__OutOfBoundsTokenId();
+        }
+        _mint(to, tokenId);
+        s_allTokensIndex[tokenId] = s_allTokens.length;
+        s_allTokens.push(tokenId);
+        s_ownedTokens[to].push(tokenId);
+        s_ownedTokensIndex[tokenId] = s_ownedTokens[to].length;
     }
 
     function tokenOfOwnerByIndex(
--- contracts/contract-2-nft-enumerable/test/EnumerableNFT.t.sol
+++ contracts/contract-2-nft-enumerable/test/EnumerableNFT.t.sol
@@ -10,6 +10,7 @@ contract EnumerableNFTTest is Test {
     NFTInfo public nftInfo;
 
     address owner = makeAddr("OWNER");
+    address user = makeAddr("USER");
 
     function setUp() public {
         vm.startPrank(owner);
@@ -24,7 +25,63 @@ contract EnumerableNFTTest is Test {
 
     function testCanMint() public {
         vm.prank(owner);
-
+        enumerableNFT.mint(owner, 1);
         assertEq(enumerableNFT.totalSupply(), 1);
+    }
+
+    function testCanGetOwnedTokens() public {
+        vm.prank(owner);
+        enumerableNFT.mint(owner, 1);
+        enumerableNFT.mint(owner, 2);
+        enumerableNFT.mint(owner, 3);
+        uint256[] memory ownedTokens = enumerableNFT.getOwnedTokens(owner);
+        assertEq(ownedTokens.length, 3);
+        assertEq(ownedTokens[0], 1);
+        assertEq(ownedTokens[1], 2);
+        assertEq(ownedTokens[2], 3);
+    }
+
+    function testCanGetPrimeBalance() public {
+        vm.startPrank(user);
+        enumerableNFT.mint(user, 1);
+        enumerableNFT.mint(user, 2);
+        enumerableNFT.mint(user, 3);
+        enumerableNFT.mint(user, 4);
+        enumerableNFT.mint(user, 5);
+        enumerableNFT.mint(user, 6);
+        enumerableNFT.mint(user, 7);
+        enumerableNFT.mint(user, 8);
+        enumerableNFT.mint(user, 9);
+        enumerableNFT.mint(user, 10);
+        enumerableNFT.mint(user, 11);
+        enumerableNFT.mint(user, 12);
+        enumerableNFT.mint(user, 13);
+        enumerableNFT.mint(user, 14);
+        enumerableNFT.mint(user, 15);
+        enumerableNFT.mint(user, 16);
+        enumerableNFT.mint(user, 17);
+        enumerableNFT.mint(user, 18);
+        enumerableNFT.mint(user, 19);
+        enumerableNFT.mint(user, 20);
+        vm.stopPrank();
+        assertEq(nftInfo.primeBalanceOf(user), 8);
+    }
+
+    function testCanGetPrimeBalanceWithNoPrimes() public {
+        vm.startPrank(user);
+        enumerableNFT.mint(user, 1);
+        enumerableNFT.mint(user, 4);
+        enumerableNFT.mint(user, 6);
+        enumerableNFT.mint(user, 8);
+        enumerableNFT.mint(user, 9);
+        enumerableNFT.mint(user, 10);
+        enumerableNFT.mint(user, 12);
+        enumerableNFT.mint(user, 14);
+        enumerableNFT.mint(user, 15);
+        enumerableNFT.mint(user, 16);
+        enumerableNFT.mint(user, 18);
+        enumerableNFT.mint(user, 20);
+        vm.stopPrank();
+        assertEq(nftInfo.primeBalanceOf(user), 0);
     }
 }
